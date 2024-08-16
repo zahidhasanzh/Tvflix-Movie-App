7af624a5911c537c61e3c763cfe59556f65f4032
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-08-16 17:01:14 +0600

    A movie app header part done

---
+++ assets/js/global.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+* Add event on multiple elements
+*/
+
+const addEventOnElements = function(elements, eventType, callback){
+    for(const elem of elements) elem.addEventListener(eventType, callback);
+}
+
+/**
+* Toggle search box in mobile device || small screen
+*/
+
+const searchBox = document.querySelector("[search-box]");
+console.log(searchBox);
+const searchTogglers = document.querySelectorAll("[search-toggler]")
+
+addEventOnElements(searchTogglers, "click", function(){
+    searchBox.classList.toggle("active")
+})
+
+
+
+
+
+
+
+
+
+
